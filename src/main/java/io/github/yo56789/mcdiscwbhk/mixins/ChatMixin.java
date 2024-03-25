306230af15b50dd0b43e8b303efe3d9223dfb9c9
yo56789 <elipav400@gmail.com>
2024-03-25 17:06:13 -0400

    Change for more compatibility

--- src/main/java/io/github/yo56789/mcdiscwbhk/mixins/ChatMixin.java
+++ src/main/java/io/github/yo56789/mcdiscwbhk/mixins/ChatMixin.java
@@ -1,20 +1,15 @@
 package io.github.yo56789.mcdiscwbhk.mixins;
 
-import io.github.yo56789.mcdiscwbhk.Main;
 import io.github.yo56789.mcdiscwbhk.WebhookHandler;
 import io.github.yo56789.mcdiscwbhk.config.Config;
-import net.minecraft.network.MessageType;
-import net.minecraft.server.PlayerManager;
+import net.minecraft.server.filter.TextStream;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.UUID;
-import java.util.function.Function;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value=ServerPlayNetworkHandler.class)
 public class ChatMixin {
@@ -22,12 +17,9 @@ public class ChatMixin {
     @Shadow
     public ServerPlayerEntity player;
 
-    @Redirect(method="handleMessage", at=@At(value = "INVOKE", target="Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Ljava/util/function/Function;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V"))
-    public void chatMessage(PlayerManager instance, Text message, Function<ServerPlayerEntity, Text> playerMessageFactory, MessageType type, UUID sender) {
-        String data = WebhookHandler.assembleString(message.getString().replaceFirst("<[^>]+>", "").trim(), player.getDisplayName().asString());
+    @Inject(method="handleMessage", at=@At(value = "INVOKE", target="Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Ljava/util/function/Function;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V"))
+    public void chatMessage(TextStream.Message message, CallbackInfo ci) {
+        String data = WebhookHandler.assembleString(message.getRaw().replaceFirst("<[^>]+>", "").trim(), player.getDisplayName().asString());
         WebhookHandler.post(Config.WEBHOOK_URI, data);
-        instance.broadcast(message, type, sender);
     }
-
-
 }
